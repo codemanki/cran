858fb368f43f50cee8764061a8dfaf5d060c4002
codemanki <me@codemanki.com>
2013-04-08 10:27:57 +0200

    Initial rails commint

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cran::Application.config.secret_token = '757395a9680feeee331d9006212c080d42436768546ebe3e322a7bd427a3fa0e1d183f01a2827ea11c76a57e68b58ff6adc11e6ecd54e27ec72e5372f4fc3bb1'
