b10df06634d2c6d37e634c66e223548a1377c80d
codemanki <me@codemanki.com>
2013-04-08 13:39:18 +0200

    Files

--- lib/cran/cran_api.rb
+++ lib/cran/cran_api.rb
@@ -23,35 +23,7 @@ module CranApi
     
     def parse_packages(content)
       return [] if !content || content.blank?
-      packages = []
-      
-      no_yes_keys = ["no", "yes"]
-      
-      content.each_line do |line|
-        parsed_line =  Dcf.parse(line)
-        if parsed_line && parsed_line[0]
-          entity = parsed_line[0]
-          
-          if(entity["Package"])
-            packages << {name: entity["Package"]}
-          else
-            #Parse key
-            key = entity.keys[0]
-            sym = key.downcase.to_sym
-            value = entity[entity.keys[0]]
-            #if this is yes/no field, return boolean
-            if no_yes_keys.include? value
-              value = value == "yes" ? true : false
-            end
-            
-            #something happened. Either no package element was created or 
-            raise PackagesListParsingError, "I have key '#{key}' with value '#{value}' but previously there was no package " if !packages[packages.length - 1]
-            raise PackagesListParsingError, "I have key '#{key}' with value '#{value}' but previously it was already defined with value '#{packages[packages.length - 1][sym]}'"  if packages[packages.length - 1][sym]
-            packages[packages.length - 1][sym] = value
-          end
-        end
-      end
-      packages
+      Dcf.parse(content)
     end
   end
   
